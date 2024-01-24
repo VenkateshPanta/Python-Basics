3c8903561a8e359cc765468ef3e7307957d0828b
VenkateshPanta <143291393+VenkateshPanta@users.noreply.github.com>
2024-01-23 20:15:27 -0500

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,20 @@
+## Basic Print Statement
+print("Hello World")
+
+##Finding data type
+a = ['124','5','236']
+print(type(a))
+
+## Basis Mutliplication
+number1 = 245
+number2 = 23
+print(number1 * number2)
+
+## Basic Addition
+print(number1+number2)
+
+## Basic Subtraction
+print(number1-number2)
+
+## Basic Divison
+print(number1/5)
